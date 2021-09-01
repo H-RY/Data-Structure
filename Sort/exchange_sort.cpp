d1ca47e3ad4de8754306c44793e3b1df569128fc
H-RY <849541922@qq.com>
2021-09-01 17:29:20 +0800

    新增了快速排序

--- Sort/exchange_sort.cpp
+++ Sort/exchange_sort.cpp
@@ -9,16 +9,19 @@ using namespace std;
 void traverse_array(int temp[]);//打印
 void bubble_sort(int temp[], int n);//冒泡排序
 void quick_sort(int temp[], int low, int high);//快速排序
+int partition(int temp[], int low, int high);//一趟划分
 
 int main(){
     int temp[max_size] = {49,38,65,97,76,13,27,49,1};//序号从1开始
-    bubble_sort(temp, 9);
+//    bubble_sort(temp, 9);
+    partition(temp, 0, 8);
+    quick_sort(temp, 0,8);
     traverse_array(temp);
     return 0;
 }
 
 void traverse_array(int temp[]){
-    for(int i = 1; i < max_size; i++){
+    for(int i = 0; i < max_size - 1; i++){
         cout<<temp[i]<<endl;
     }
 }
@@ -39,3 +42,27 @@ void bubble_sort(int temp[], int n){
         }
     }
 }
+
+int partition(int temp[], int low, int high){
+    int pivot = temp[low];
+    while(low < high){
+        while(low < high && temp[high] >= pivot)
+            high--;
+        temp[low] = temp[high];
+        while(low < high && temp[low] <= pivot)
+            low++;
+        temp[high] = temp[low];
+    }
+    temp[low] = pivot;
+    return low;
+}
+
+void quick_sort(int temp[], int low, int high){
+    if(low < high){
+        //partition()是划分操作，将表 temp[low] 到  temp[high] 划分为满足上述条件的两个子表
+        int pivotpos = partition(temp, low, high);
+        quick_sort(temp, low, pivotpos - 1);
+        quick_sort(temp, pivotpos + 1, high);
+    }
+}
+
